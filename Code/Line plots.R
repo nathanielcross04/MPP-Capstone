f504093c6a431556138f0783e7d18979b447e04c
Nathaniel Cross <ndmcross@gmail.com>
2026-04-22 12:32:57 -0700

    Cutting one line plot

--- Code/Line plots.R
+++ Code/Line plots.R
@@ -36,14 +36,15 @@ p1 <- ggplot(DATA_PATH, aes(x = year, y = n_medoids)) +
   scale_x_continuous(breaks = c(2000, 2005, 2010, 2015, 2019)) +
   scale_y_continuous(breaks = seq(0, 9, by = 2), limits = c(0, 8)) +
   labs(
-    subtitle = "Number of medoids equidistant to the centroid",
+    title = "Number of medoids equidistant to the centroid\ndecreases sharply from 2000 to 2019",
     x        = NULL,
     y        = NULL
   ) +
   theme_minimal(base_family = "lato") +
   theme(
-    plot.subtitle        = element_text(size = 40, color = "black", family = "lato",
-                                        hjust = 0),
+    plot.title           = element_text(size = 60, color = "black", face = "bold",
+                                        family = "lato", lineheight = 0.3,
+                                        hjust = 0, margin = margin(t = 10, b = 10)),
     axis.text            = element_text(size = 35, color = "#444444"),
     panel.grid.major     = element_line(color = "#CCCCCC", linewidth = 0.4),
     panel.grid.minor     = element_blank(),
@@ -51,6 +52,17 @@ p1 <- ggplot(DATA_PATH, aes(x = year, y = n_medoids)) +
     plot.margin          = margin(t = 5, r = 20, b = 10, l = 20),
     plot.title.position = "plot"
   )
+
+
+
+# Save plot
+ggsave(
+  filename = file.path(OUTPUT_DIR, "medoids_combined.png"),
+  width    = 7.5,
+  height   = 5,
+  dpi      = 300,
+  bg       = "white"
+)
 
 p2 <- ggplot(DATA_PATH, aes(x = year, y = min_dist)) +
   geom_line(color = "#56B4E9", linewidth = 0.9) +
